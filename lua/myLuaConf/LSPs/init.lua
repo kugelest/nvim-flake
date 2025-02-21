22803ef18e21bcf967f5bcd939c77dc58abdd80f
Kugele Stefan <skugele@baumer.com>
2025-02-21 15:57:24 +0100

    eslint

--- lua/myLuaConf/LSPs/init.lua
+++ lua/myLuaConf/LSPs/init.lua
@@ -69,8 +69,10 @@ if nixCats('neonixdev') then
 	end
 end
 
+
 if nixCats('react') then
 	-- servers.ts_ls = {}
+
 	servers.eslint = {
 		settings = {
 			experimental = {
@@ -79,32 +81,7 @@ if nixCats('react') then
 			format = false,
 		}
 	}
-	-- servers.html = { filetypes = { 'html', 'twig', 'hbs' } }
 
-
-	-- local languages = require('efmls-configs.defaults').languages()
-	-- languages = vim.tbl_extend('force', languages, {
-	-- 	-- Custom languages, or override existing ones
-	-- 	javascriptreact = {
-	-- 		require('efmls-configs.linters.eslint'),
-	-- 		require('efmls-configs.formatters.prettier'),
-	-- 	},
-	-- })
-	-- servers.efm = {
-	-- 	filetypes = vim.tbl_keys(languages),
-	-- 	settings = {
-	-- 		rootMarkers = { ".git/" },
-	-- 		languages = languages,
-	-- 	},
-	-- 	init_options = {
-	-- 		documentFormatting = true,
-	-- 		documentRangeFormatting = true,
-	-- 		hover = true,
-	-- 		documentSymbol = true,
-	-- 		codeAction = true,
-	-- 		completion = true
-	-- 	},
-	-- }
 	local null_ls = require("null-ls")
 	null_ls.setup({
 		sources = {
@@ -112,66 +89,13 @@ if nixCats('react') then
 		},
 		on_attach = require('myLuaConf.LSPs.caps-on_attach').on_attach,
 	})
-
-
-	-- require('lspconfig').efm.setup(vim.tbl_extend('force', efmls_config, {
-	-- 	on_attach = require('myLuaConf.LSPs.caps-on_attach').on_attach,
-	-- 	-- on_attach = on_attach,
-	-- 	-- capabilities = capabilities,
-	-- }))
-
-	-- local prettier = require('efmls-configs.formatters.prettier')
-	-- local languages = {
-	-- 	javascript = { prettier },
-	-- 	javascriptreact = { prettier },
-	-- }
-	-- require('lspconfig').efm.setup({
-	-- 	-- on_attach = function(event) require('myLuaConf.LSPs.caps-on_attach').on_attach(vim.lsp.get_client_by_id(event.data.client_id), event.buf) end,
-	-- 	init_options = {
-	-- 		documentFormatting = true,
-	-- 		documentRangeFormatting = true,
-	-- 	},
-	-- 	filetypes = vim.tbl_keys(languages),
-	-- 	settings = {
-	-- 		rootMarkers = { '.prettierrc' },
-	-- 		languages = languages,
-	-- 	},
-	-- })
 end
 
--- This is this flake's version of what kickstarter has set up for mason handlers.
--- This is a convenience function that calls lspconfig on the lsps we downloaded via nix
--- This will not download your lsp. Nix does that.
-
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---  All of them are listed in https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
---  You may do the same thing with cmd
-
--- servers.clangd = {},
--- servers.gopls = {},
--- servers.pyright = {},
--- servers.rust_analyzer = {},
 
 
 if not require('nixCatsUtils').isNixCats and nixCats('lspDebugMode') then
 	vim.lsp.set_log_level("debug")
 end
--- If you were to comment out this autocommand
--- and instead pass the on attach function directly to
--- nvim-lspconfig, it would do the same thing.
--- come to think of it, it might be better because then lspconfig doesnt have to be called before lsp attach?
--- but you would still end up triggering on a FileType event anyway, so, it makes little difference.
--- vim.api.nvim_create_autocmd('LspAttach', {
--- 	group = vim.api.nvim_create_augroup('nixCats-lsp-attach', { clear = true }),
--- 	callback = function(event)
--- 		require('myLuaConf.LSPs.caps-on_attach').on_attach(vim.lsp.get_client_by_id(event.data.client_id),
--- 			event.buf)
--- 	end
--- })
 
 require('lze').load {
 	{
@@ -187,10 +111,7 @@ require('lze').load {
 			if require('nixCatsUtils').isNixCats then
 				for server_name, cfg in pairs(servers) do
 					require('lspconfig')[server_name].setup({
-						init_options = (cfg or {}).init_options,
-						capabilities = require('myLuaConf.LSPs.caps-on_attach').get_capabilities(
-							server_name),
-						-- this line is interchangeable with the above LspAttach autocommand
+						capabilities = require('myLuaConf.LSPs.caps-on_attach').get_capabilities(server_name),
 						on_attach = require('myLuaConf.LSPs.caps-on_attach').on_attach,
 						settings = (cfg or {}).settings,
 						filetypes = (cfg or {}).filetypes,
@@ -208,8 +129,7 @@ require('lze').load {
 					function(server_name)
 						require('lspconfig')[server_name].setup {
 							capabilities = require('myLuaConf.LSPs.caps-on_attach').get_capabilities(server_name),
-							-- this line is interchangeable with the above LspAttach autocommand
-							-- on_attach = require('myLuaConf.LSPs.caps-on_attach').on_attach,
+							on_attach = require('myLuaConf.LSPs.caps-on_attach').on_attach,
 							settings = (servers[server_name] or {}).settings,
 							filetypes = (servers[server_name] or {}).filetypes,
 						}
